0703299e1560c773877cf96bb7362f43e015fed6
Akash Das <aakashdas368@gmail.com>
2020-08-19 09:36:07 +0530

    🥚 change AlertDialog box's style

--- lib/shared/AlertDialog.dart
+++ lib/shared/AlertDialog.dart
@@ -3,13 +3,8 @@ import 'package:flutter/material.dart';
 import '../styles/theme.dart';
 
 void showAlertDialog(BuildContext context, String title, String message) {
-  // AlertDialog alertDialog = AlertDialog(
-  //   title: Text(title),
-  //   content: Text(message),
-  // );
-
   Dialog alertDialog = Dialog(
-    backgroundColor: bgSecondary,
+    // backgroundColor: bgSecondary,
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(10.0),
     ),
@@ -22,11 +17,11 @@ void showAlertDialog(BuildContext context, String title, String message) {
         children: <Widget>[
           Padding(
             padding: EdgeInsets.all(15.0),
-            child: Text(title, style: h2),
+            child: Text(title, style: dialogTitleTextStyle),
           ),
           Padding(
             padding: EdgeInsets.all(15.0),
-            child: Text(message, style: h4),
+            child: Text(message, style: dialogHintTextStyle),
           ),
         ],
       ),
