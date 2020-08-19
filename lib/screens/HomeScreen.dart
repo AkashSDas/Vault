19710d39bb56bb45779f5be02d983ae7fadb5cd7
Akash Das <aakashdas368@gmail.com>
2020-08-19 11:43:06 +0530

    🍕 solve scrolling issue of HomeScreen

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -78,6 +78,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _buildAccountList() {
     return ListView.builder(
+      primary: false,
       scrollDirection: Axis.vertical,
       shrinkWrap: true,
       itemCount: count,
